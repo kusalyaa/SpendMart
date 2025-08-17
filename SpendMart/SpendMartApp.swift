d3dc4a5ba1cf682443e3dfae4a1a364a5e132d19
kusalyaa <kusalya70@gmail.com>
2025-08-17 10:01:53 +0530

    Initial Commit

---
+++ SpendMart/SpendMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendMartApp.swift
+//  SpendMart
+//
+//  Created by  			Kusalya 023 on 2025-08-17.
+//
+
+import SwiftUI
+
+@main
+struct SpendMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpendMartTests/SpendMartTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpendMartTests.swift
+//  SpendMartTests
+//
+//  Created by  			Kusalya 023 on 2025-08-17.
+//
+
+import Testing
+@testable import SpendMart
+
+struct SpendMartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
